3d97b9d48fdf55f2924be08b68b1c5e6036132d5
Neil Cruz <neilbcruz@gmail.com>
2022-12-02 17:41:33 -0500

    add favicon and change title and description

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet'
 
 import PageHeader from './components/PageHeader/PageHeader';
 import HomePage from './pages/HomePage/HomePage';
@@ -9,6 +10,12 @@ function App() {
   return (
     <>
       <div className='app'>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Neil Cruz Portfolio</title>
+          <link rel="canonical" href="https://neilbcruz.com/" />
+          <meta name="description" content="Neil Cruz's Portfolio" />
+        </Helmet>
         <Router>
           <PageHeader />
           <Routes>
