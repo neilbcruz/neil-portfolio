8eb748781b775ed57bcbf89c828836265180ca43
Neil Cruz <neilbcruz@gmail.com>
2023-02-10 14:07:36 -0500

    update anime and games section

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,6 +1,10 @@
 import './Games.scss';
 
 export default function Games() {
+    const newTab = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    }
+
     return (
         <>
             <div className='games' id='games'>
@@ -14,20 +18,26 @@ export default function Games() {
                 <div className='games__container'>
                     <div className='games__card'>
                         <div className='games__card-info'>
-                            <h3>Star Ocean: The Divine Force</h3>
-                            <img src={'https://pbs.twimg.com/media/FcsjXZIWYAYjlYm.jpg:large'} alt='Star Ocean 6 cover' />
+                            <h3>Stranger of Paradise: FFO</h3>
+                            <img onClick={() => newTab('https://www.square-enix-games.com/sopffo/en-us/')} src={'https://assets-prd.ignimgs.com/2022/02/15/stranger-of-paradise-button-fn-1644883544038.jpg'} alt='FF Origins cover' />
                         </div>
                     </div>
                     <div className='games__card'>
                         <div className='games__card-info'>
-                            <h3>Pokemon Violet</h3>
-                            <img src={'https://assets-prd.ignimgs.com/2022/08/03/pokemon-violet-1659542326365.jpg'} alt='Pokemon Violet cover' />
+                            <h3>Guilty Gear -STRIVE-</h3>
+                            <img onClick={() => newTab('https://www.guiltygear.com/ggst/en/')} src={'https://assets-prd.ignimgs.com/2021/04/09/guilty-gear-strive-1617935028610.jpg'} alt='GG STRIVE cover' />
                         </div>
                     </div>
                     <div className='games__card'>
                         <div className='games__card-info'>
-                            <h3>Xenoblade Chronicles 3</h3>
-                            <img src={'https://images.nintendolife.com/1b9a21fa2c5ad/xenoblade-chronicles-3.original.jpg'} alt='Xenoblade 3 cover' />
+                            <h3>Fire Emblem ENGAGE</h3>
+                            <img onClick={() => newTab('https://www.nintendo.com/en-ca/store/products/fire-emblem-engage-switch/')} src={'https://assets.nintendo.com/image/upload/ar_16:9,c_limit,w_656/b_white/f_auto/q_auto/ncom/en_US/games/switch/f/fire-emblem-engage-switch/hero'} alt='Xenoblade 3 cover' />
+                        </div>
+                    </div>
+                    <div className='games__card'>
+                        <div className='games__card-info'>
+                            <h3>Hogwarts Legacy</h3>
+                            <img onClick={() => newTab('https://www.hogwartslegacy.com/en-ca')} src={'https://assets-prd.ignimgs.com/2022/05/24/hogwarts-legacy-button-fin-1653421326559.jpg'} alt='Hogwarts Legacy cover' />
                         </div>
                     </div>
                 </div>
